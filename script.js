a8c746af205019408a57a451ec9e188f250f3aa6
ashan <indrajee79@gmail.com>
2022-11-27 23:08:01 +0530

    js styling fun

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ console.log(evenDivs)
 
 const oddDivs = view1.querySelectorAll("div:nth-of-type(3n)");
 console.log(oddDivs)
+
+for (let i = 0; i < evenDivs.length; i++) {
+    evenDivs[i].style.backgroundColor = "maroon";
+    //evenDivs[i].style.width = "200px";
+    //evenDivs[i].style.height = "200px";
+}
+
+const navText = document.querySelector("nav h1");
+console.log(navText);
+navText.textContent = "Hello World!"
+const navbar = document.querySelector("nav");
+navbar.innerHTML= "<h1>Hello!</h1> <p>This Should Align Right</p>";
+navbar.style.color = "maroon";
