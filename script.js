31b1b3ad73265e84227909515c67bac02b025558
ashan <indrajee79@gmail.com>
2022-11-27 22:52:07 +0530

    js tests

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ console.log(view2)
 
 view1.style.display = "flex";
 view2.style.display = "flex";
+
+const views = document.getElementsByClassName("view")
+console.log(views)
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
+
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+
+const sameDivs = view1.getElementsByTagName("div");
+console.log(sameDivs);
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)")
+console.log(evenDivs)
+
+const oddDivs = view1.querySelectorAll("div:nth-of-type(3n)");
+console.log(oddDivs)
